0911a5bc8127cf7dac9a08fb6950da30e3ba4393
Park Byeong Jin <67573062+fatejin@users.noreply.github.com>
2020-09-13 00:15:29 +0900

    13. JavaScript- Object-basic1

---
+++ object.js
@@ -0,0 +1,9 @@
+var members = ['egoing', 'k8805', 'hoya'];
+console.log(members[1]); //array k8805
+
+var roles = {
+    'programmer': 'egoing'
+    'desingner': 'k8805',
+    'manager': 'hoya'
+} //객체는 각각의 데이터 마다 고유 명칭을 적어준다.
+console.log(roles.desingner); //k8805
